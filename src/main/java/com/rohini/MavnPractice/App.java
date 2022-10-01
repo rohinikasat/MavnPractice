353d221032644cc984a3a67e3bf69fb08c15a58c
Rohini Kasat <rohinikasat@gmail.com>
2022-09-30 23:07:40 -0700

    Initial commit

---
+++ src/main/java/com/rohini/MavnPractice/App.java
@@ -0,0 +1,13 @@
+package com.rohini.MavnPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
